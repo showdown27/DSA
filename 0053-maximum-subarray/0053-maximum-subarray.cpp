97fb1915c64dce4a05b727703f71aaaa07cf49cb
showdown27 <154733861+showdown27@users.noreply.github.com>
2026-09-05 02:13:53 +0530

    Time: 0 ms (100%), Space: 71.7 MB (52.55%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& a) {
-        int be = a[0], v1 = 0, v2 = 0;
-        int res = a[0];
-        for(int i = 1; i<a.size(); i++){
+        int res = a[0], v1 = 0, v2 = 0, be = a[0];
+        for(int i = 1; i< a.size(); i++){
             v1 = be + a[i];
             v2 = a[i];
             be = max(v1,v2);
-            res = max(be, res);
+            res = max(res, be);
         }
         return res;
     }
