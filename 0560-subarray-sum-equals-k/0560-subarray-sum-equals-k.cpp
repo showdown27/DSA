02e513628325c7bc7d6d2b993539cfa7043d3e18
showdown27 <154733861+showdown27@users.noreply.github.com>
2026-09-05 02:38:50 +0530

    Time: 43 ms (57.2%), Space: 45.5 MB (55.33%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int subarraySum(vector<int>& a, int k) {
-        unordered_map<int, int>m;
+        unordered_map<int, int> m;
         m[0]++;
-        int count = 0, sum = 0;
-        for(int i = 0; i<a.size(); i++){
+        int sum = 0, target = 0, count = 0;
+        for(int i = 0; i< a.size(); i++){
             sum = sum + a[i];
-            int target = sum - k;
+            target = sum - k;
             if(m.find(target)!=m.end()){
                 count = count + m[target];
             }
@@ -14,4 +14,4 @@ public:
         }
         return count;
     }
-};      
+};
